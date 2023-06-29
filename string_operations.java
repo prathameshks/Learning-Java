738547497476ec1eb533c8b50a507be1fa6a6e2d
Prathamesh <prathameshks2003@gmial.com>
2023-06-29 23:07:38 +0530

    string methods

--- string_operations.java
+++ string_operations.java
@@ -48,5 +48,42 @@ public class string_operations {
         for (String i : s2) {
             System.out.println("\'" + i + "\'"); // print each element of s2 surrounded by single quotes
         }
+
+        // Additional string methods examples
+        System.out.println(name.startsWith("Pra")); // check if name starts with the substring "Pra"
+        System.out.println(name.isEmpty()); // check if name is an empty string
+        System.out.println(String.join("-", s1, s)); // join s1 and s with a delimiter "-"
+
+        // typecasting
+        // Convert an int to a string
+        int num = 123;
+        String numStr = String.valueOf(num);
+        System.out.println(numStr);
+
+        // Convert a long to a string
+        long longNum = 123456789L;
+        String longNumStr = String.valueOf(longNum);
+        System.out.println(longNumStr);
+
+        // Convert a float to a string
+        float floatNum = 1.23f;
+        String floatNumStr = String.valueOf(floatNum);
+        System.out.println(floatNumStr);
+
+        // Convert a double to a string
+        double doubleNum = 1.23456789;
+        String doubleNumStr = String.valueOf(doubleNum);
+        System.out.println(doubleNumStr);
+
+        // Convert a boolean to a string
+        boolean boolVal = true;
+        String boolValStr = String.valueOf(boolVal);
+        System.out.println(boolValStr);
+
+        // Convert a char to a string
+        char charVal = 'A';
+        String charValStr = String.valueOf(charVal);
+        System.out.println(charValStr);
+
     }
 }
