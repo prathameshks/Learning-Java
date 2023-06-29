23384cdbed0ecd5f4942f8d233c53274cec2c535
Prathamesh <prathameshks2003@gmial.com>
2023-06-29 11:27:53 +0530

    operator in java

--- operators.java
+++ operators.java
@@ -139,8 +139,8 @@ public class operators {
         System.out.print("true || true = ");
         System.out.println(true || true);
 
-        System.out.print("true || false = ");
-        System.out.println(true || false);
+        System.out.print("false || true = ");
+        System.out.println(false || true);
 
         System.out.print("false || false = ");
         System.out.println(false || false);
